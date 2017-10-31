02ce9fff671f38e83000e21a13efa7f3fe77d35c
Kevin Caravaggio <kevvurs@Galago686>
2017-10-31 12:24:34 -0700

    added I and D type encodings

--- instr_decoder.sv
+++ instr_decoder.sv
@@ -2,11 +2,11 @@ module instr_decoder(instruction, UncondBr, BrTaken, Reg2Loc, RegWrite, ALUSrc, 
 	input logic instruction;
 	output logic UncondBr, BrTaken, Reg2Loc,
 		RegWrite, ALUSrc, ALUOp, MemWrite;
-	
+
 	parameter
 		// B-type
 		B = 6'b000101,
-		
+
 		// CB-type
 		BCond = 8'b01010100,
 		CBZ = 8'b10110100,
@@ -17,7 +17,7 @@ module instr_decoder(instruction, UncondBr, BrTaken, Reg2Loc, RegWrite, ALUSrc, 
 		LT = 8'b00001011,
 		GT = 8'b00001100,
 		LE = 8'b00001101,
-		
+
 		// R-type
 		AND = 11'b10001010000,
 		ADD = 11'b10001011000,
@@ -32,17 +32,27 @@ module instr_decoder(instruction, UncondBr, BrTaken, Reg2Loc, RegWrite, ALUSrc, 
 		BR = 11'b11010110000,
 		ANDS = 11'b11101010000,
 		SUBS = 11'b11101011000,
-		
+
 		// I-type
 		ADDI = 10'b1001000100,
-		
+		ANDI = 10'b1001001000,
+		ADDIS = 10'b0101100100,
+		ORRI = 10'b0101101000,
+		SUBI = 10'b1101000100,
+		EORI = 10'b1101001000,
+		SUBIS = 10'b1011000100,
+		ANDIS = 10'b1011001000,
+
 		// D-type
-		STRUB = 11'b00111000000;
-		
-		
-		
-		
-	
+		STURB = 11'b00111000000,
+		LDURB = 11'b00111000010,
+		STUR = 11'b11111000000,
+		LDUR = 11'b11111000010;
+
+		// MOVK MOVZ
+
+
+
 	always_comb
 		// B-type
 		case (instruction[31:26])
@@ -57,7 +67,7 @@ module instr_decoder(instruction, UncondBr, BrTaken, Reg2Loc, RegWrite, ALUSrc, 
 					end
 					default:
 						//R-type
-						
+
 				endcase
 		endcase
 endmodule
