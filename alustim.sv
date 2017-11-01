615b97250728913131e549f94e26275bfc56ce98
Daniel Huynh <huynh14@uw.edu>
2017-11-01 11:38:23 -0700

    fixed

--- alustim.sv
+++ alustim.sv
@@ -1,3 +1,4 @@
+/*
 // Test bench for ALU
 `timescale 1ns/10ps
 
@@ -114,3 +115,124 @@ module alustim();
 		
 	end
 endmodule
+
+*/
+
+
+// Test bench for ALU - TA version
+`timescale 1ns/10ps
+
+// Flags:
+// negative: whether the result output is negative if interpreted as 2's comp.
+// zero: whether the result output was a 64-bit zero.
+// overflow: on an add or subtract, whether the computation overflowed if the inputs are interpreted as 2's comp.
+// carry_out: on an add or subtract, whether the computation produced a carry-out.
+
+// cntrl			Operation						Notes:
+// 000:			result = B						value of overflow and carry_out unimportant
+// 010:			result = A + B
+// 011:			result = A - B
+// 100:			result = bitwise A & B		value of overflow and carry_out unimportant
+// 101:			result = bitwise A | B		value of overflow and carry_out unimportant
+// 110:			result = bitwise A XOR B	value of overflow and carry_out unimportant
+
+module alustim();
+
+	parameter delay = 100000;
+
+	logic		[63:0]	A, B;
+	logic		[2:0]		cntrl;
+	logic		[63:0]	result;
+	logic					negative, zero, overflow, carry_out ;
+
+	parameter ALU_PASS_B=3'b000, ALU_ADD=3'b010, ALU_SUBTRACT=3'b011, ALU_AND=3'b100, ALU_OR=3'b101, ALU_XOR=3'b110;
+	
+
+	alu dut (.A, .B, .cntrl, .result, .negative, .zero, .overflow, .carry_out);
+
+	// Force %t's to print in a nice format.
+	initial $timeformat(-9, 2, " ns", 10);
+
+	integer i;
+	logic [63:0] test_val;
+	initial begin
+	
+		$display("%t testing PASS_B operations", $time);
+		cntrl = ALU_PASS_B;
+		for (i=0; i<100; i++) begin
+			A = $random(); B = $random();
+			#(delay);
+			assert(result == B && negative == B[63] && zero == (B == '0));
+		end
+
+		$display("%t testing random adds and subtracts", $time);
+		for (i=0; i<200; i++) begin
+			A = $random(); B = $random();
+			cntrl = ($random & 2'b01) ? ALU_ADD : ALU_SUBTRACT;
+			if (cntrl == ALU_ADD)
+				test_val = A + B;
+			else
+				test_val = A - B;
+			#(delay);
+			assert(test_val == result);
+			assert(test_val[63] == negative && (test_val == '0) == zero);
+		end
+		
+		$display("%t testing random logic operations", $time);
+		for (i=0; i<300; i++) begin
+			A = $random(); B = $random();
+			for(cntrl = $random(); cntrl != ALU_AND && cntrl != ALU_OR && cntrl != ALU_XOR; cntrl = $random());
+			if (cntrl == ALU_AND)
+				test_val = A & B;
+			else if (cntrl == ALU_OR)
+				test_val = A | B;
+			else
+				test_val = A ^ B;
+			#(delay);
+			assert(test_val == result);
+			assert(test_val[63] == negative && (test_val == '0) == zero);
+		end
+		
+		$display("%t testing adds and overflow, carry-out", $time);
+		cntrl = ALU_ADD;
+		A = 64'hFFFFFFFFFFFFFFFF; B = 64'h0000000000000001;
+		#(delay);
+		assert(result == 64'h0000000000000000 && carry_out == 1 && overflow == 0 && negative == 0 && zero == 1);
+		
+		A = 64'h3FFFFFFFFFFFFFFF; B = 64'h0000000000000001;
+		#(delay);
+		assert(result == 64'h4000000000000000 && carry_out == 0 && overflow == 0 && negative == 0 && zero == 0);
+		
+		A = 64'h7FFFFFFFFFFFFFFF; B = 64'h0000000000000001;
+		#(delay);
+		assert(result == 64'h8000000000000000 && carry_out == 0 && overflow == 1 && negative == 1 && zero == 0);
+
+		A = 64'h7FFFFFFFFFFFFFFF; B = 64'hFFFFFFFFFFFFFFFF;
+		#(delay);
+		assert(result == 64'h7FFFFFFFFFFFFFFE && carry_out == 1 && overflow == 0 && negative == 0 && zero == 0);
+
+		A = 64'h8000000000000000; B = 64'hFFFFFFFFFFFFFFFF;
+		#(delay);
+		assert(result == 64'h7FFFFFFFFFFFFFFF && carry_out == 1 && overflow == 1 && negative == 0 && zero == 0);
+		
+		
+		$display("%t testing subtracts and overflow, carry-out", $time);
+		cntrl = ALU_SUBTRACT;
+		A = 64'h3FFFFFFFFFFFFFFF; B = 64'hFFFFFFFFFFFFFFFF;
+		#(delay);
+		assert(result == 64'h4000000000000000 && carry_out == 0 && overflow == 0 && negative == 0 && zero == 0);
+		
+		A = 64'h7FFFFFFFFFFFFFFF; B = 64'hFFFFFFFFFFFFFFFF;
+		#(delay);
+		assert(result == 64'h8000000000000000 && carry_out == 0 && overflow == 1 && negative == 1 && zero == 0);
+		
+		A = 64'h8000000000000000; B = 64'h0000000000000001;
+		#(delay);
+		assert(result == 64'h7FFFFFFFFFFFFFFF && carry_out == 1 && overflow == 1 && negative == 0 && zero == 0);
+
+		A = 64'hDEADBEEFDECAFBAD; B = 64'hDEADBEEFDECAFBAD;
+		#(delay);
+		assert(result == 64'h0000000000000000 && carry_out == 1 && overflow == 0 && negative == 0 && zero == 1);
+	end
+endmodule
+
--- zeroFlagCheck.sv
+++ zeroFlagCheck.sv
@@ -3,20 +3,22 @@
 module zeroFlagCheck(zeroFlagCheck, result);
 	input logic [63:0] result;
 	output logic zeroFlagCheck;
-	logic [7:0] out;
-	logic [1:0] out2;
-	parameter delay = 5;
+	logic [15:0] out;
+	logic [3:0] out2;
+	parameter delay = 0.05;
 	
 	genvar i;
 	generate
-		for (i=0; i< 8; i++) begin : each
+		for (i=0; i< 16; i++) begin : each
 			or #delay ior (out[i], result[i * 4], result[(i * 4) + 1], result[(i * 4) + 2], result[(i * 4) + 3]);
 		end
 	endgenerate
 	
 	or #delay FUCC (out2[0], out[0], out[1], out[2], out[3]);
 	or #delay FUCC2 (out2[1], out[4], out[5], out[6], out[7]);
-	nor #delay FinalFUCC (zeroFlagCheck, out2[0], out2[1]);
+	or #delay FUCC3 (out2[2], out[8], out[9], out[10], out[11]);
+	or #delay FUCC4 (out2[3], out[12], out[13], out[14], out[15]);
+	nor #delay FinalFUCC (zeroFlagCheck, out2[0], out2[1], out2[2], out2[3]);
 
 endmodule
 
